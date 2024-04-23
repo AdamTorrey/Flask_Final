1e6229f305154c88cd788db00659d32d4d086431
AdamTorrey <21atorrey@gmail.com>
2024-04-22 21:22:17 -0400

    Made the website functional with links between pages, forms, and automatically updating database entries/tables. Next I need to make it look good

--- Project.py
+++ Project.py
@@ -9,7 +9,45 @@ Bootstrap(app)
 def home():
     return render_template("home.html")
 
+@app.route('/reservations')
+def reservations():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
 
+    cur = con.cursor()
+    cur.execute("select * from reservations")
+
+    rows = cur.fetchall();
+    return render_template("reserved.html", rows = rows)
+
+@app.route('/reserveform')
+def reserve_form():
+    return render_template('reserve_form.html')
+
+@app.route('/reserverecord', methods = ['post'])
+def reserve_record():
+    try:
+        name = request.form['name']
+        roomNum = request.form['roomNum']
+        checkIn = request.form['checkIn']
+        checkOut = request.form['checkOut']
+        email = request.form['email']
+
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            q = "INSERT INTO reservations (name, roomNum, checkIn, checkOut, email) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')".format(name, roomNum, checkIn, checkOut, email)
+            cur.execute(q)
+
+            con.commit()
+            msg = "Thank you '{0}' for choosing to stay with us. Your room '{1}' has successfully been reserved!".format(name, roomNum)
+
+    except:
+        con.rollback()
+        msg = "We're sorry. Your reservation has not gone through. Please try again!"
+
+    finally:
+        return render_template("reserve_record.html", msg = msg)
+        con.close
 
 if __name__ == "__main__":
     app.run()
