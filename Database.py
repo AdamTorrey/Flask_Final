1e18d4dd04c63c498bbff5ead928b2dcf5f45e73
AdamTorrey <123424035+AdamTorrey@users.noreply.github.com>
2024-04-23 09:58:38 -0400

    Delete Database.py

--- Database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-conn = sql.connect("database.db")
-print("Database created")
-
-conn.execute("CREATE TABLE reservations (name TEXT, roomNum INTEGER, checkIn TEXT, checkOut TEXT, email TEXT)")
-print("Table created")
-
-conn.close()
